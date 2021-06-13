2582b5cec3ec2e8a7486b3958236c26abfc7157e
Karson <kars.kalt@gmail.com>
2021-06-13 13:18:13 -0400

    Added username validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :scores
 
     validates :username, length: { in: 4..12 }
+    validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "must contain no spaces or special characters"}
+    validates :username, uniqueness: true
 
     def average_score
         self.scores.sum {|score| score.points} / self.scores.count
