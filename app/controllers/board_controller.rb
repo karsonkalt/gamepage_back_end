dff5c3b9e9c2188f4d8e894f91646bfced31a5e6
Karson <kars.kalt@gmail.com>
2021-06-19 18:50:48 -0400

    Updated schema to run with sessions, but sessions not persisting.

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,27 +1,43 @@
 class BoardController < ApplicationController
 
     def new
-        @@board  = Board.new(first_move_color: "black", passive_move_color: "white" )
-        @@user_b = OthelloRuby::Game::User.new("black")
-        @@user_w = OthelloRuby::Game::User.new("white")
-
+        #TODO can I use the session hash to store this?
+        session[:board] = Board.new(first_move_color: "black", passive_move_color: "white" )
+        session[:user_b] = OthelloRuby::Game::User.new("black")
+        session[:user_w] = OthelloRuby::Game::User.new("white")
+        # @@board  = Board.new(first_move_color: "black", passive_move_color: "white" )
+        # @@user_b = OthelloRuby::Game::User.new("black")
+        # @@user_w = OthelloRuby::Game::User.new("white")
+        byebug
         show
     end
 
     def show
         #TODO ActiveRecord is firing for some reason?
-        render json: @@board.values
+        render json: session[:board].values
+        # render json: @@board.values
     end
 
     def play
         cell = params[:id]
+        byebug
 
-        if @@board.set(current_user, cell)
-            render json: @@board.cells_to_be_flipped
+        if session[:board].set(current_user, cell)
+            render json: session[:board].cells_to_be_flipped
         else
-            # This line isn't working
             render json: {error: "You can't play here"}
         end
+
+
+
+        # if @@board.set(current_user, cell)
+        #     render json: @@board.cells_to_be_flipped
+        # else
+        #     render json: {error: "You can't play here"}
+        # end
+
+
+
 
         #TODO figure out currentplayer method, should be a helper but tied to this class since I'm using class variables?
         # current_player = @@user_b
@@ -37,11 +53,20 @@ class BoardController < ApplicationController
 
     private
 
+    # def current_user
+    #     if @@board.current_user == "black"
+    #         @@user_b
+    #     else
+    #         @@user_w
+    #     end
+    # end
+
     def current_user
-        if @@board.current_user == "black"
-            @@user_b
+        if session[:board].current_user == "black"
+            session[:user_b]
         else
-            @@user_w
+            session[:user_w]
         end
     end
+
 end
