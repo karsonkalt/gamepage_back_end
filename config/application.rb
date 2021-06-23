f61a74436262c3587a781f8e81a89836e6f573a3
Karson <kars.kalt@gmail.com>
2021-06-23 19:08:44 -0400

    Removed comments from application.rb.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ Bundler.require(*Rails.groups)
 module Gamepage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
@@ -35,10 +36,8 @@ module Gamepage
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
 
-    # config.middleware.use ActionDispatch::Cookies
-    # config.middleware.use ActionDispatch::Session::CookieStore, key: '_coookie_name', expire_after: 30.days
+    config.api_only = true
 
   end
 end
