9d96ffc7f0174c418f36fecec041f93c577a5eea
Karson <kars.kalt@gmail.com>
2021-06-23 18:58:42 -0400

    Removed comments from ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::API
 
-    # include ::ActionController::Cookies
-    
-    # before_action :persist_session
-
-    # private
-
-    # def persist_session
-    #     session[:init] = true
-    # end
-
 end
